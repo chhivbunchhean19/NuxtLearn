276df56b1ecef4d2f18d2f727491fd8a9745ee1c
Bunchhean chhiv <chhivchhean84@gmail.com>
2022-12-02 14:28:30 +0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>This is the testing</p>
+        <div>{{data}}</div>
+    </div>
+</template>
+
+<script setup>
+    const { data} = await useFetch('/api/ninja')
+</script>
+
+<style scoped>
+ h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+ }
+ p{
+    margin: 20px 0;
+ }
+</style>
